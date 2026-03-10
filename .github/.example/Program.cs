cfac807b1ecdc0374987edd3a27d0637fdb8742e
Simon Schulte <github@neokil.de>
2026-03-10 17:49:15 +0100

    add additional test

--- .github/.example/Program.cs
+++ .github/.example/Program.cs
@@ -30,12 +30,15 @@ namespace _example
             // Integration API example to send a session update
             // ************************************************
 
+            Console.WriteLine("Testing session update");
+
             // Create the Integration API instance using the factory
             var integrationApi = apiFactory.Create<IIntegrationApi>();
             var customerSessionId = "my_unique_session_integration_id_2";  // string | The custom identifier for this session, must be unique within the account.
 
             // Preparing a NewCustomerSessionV2 object
-            NewCustomerSessionV2 customerSession = new NewCustomerSessionV2 {
+            NewCustomerSessionV2 customerSession = new NewCustomerSessionV2
+            {
                 ProfileId = "PROFILE_ID",
                 CouponCodes = new List<string> {
                     "Cool-Stuff-2020"
@@ -61,12 +64,12 @@ namespace _example
             // Instantiating an IntegrationRequest object
             IntegrationRequest body = new IntegrationRequest(
                 customerSession
-                // Optional list of requested information to be present on the response.
-                // See src/TalonOneSdk/Model/IntegrationRequest#ResponseContentEnum for full list of supported values
-                // new List<IntegrationRequest.ResponseContentEnum> {
-                //     IntegrationRequest.ResponseContentEnum.CustomerSession,
-                //     IntegrationRequest.ResponseContentEnum.CustomerProfile
-                // }
+            // Optional list of requested information to be present on the response.
+            // See src/TalonOneSdk/Model/IntegrationRequest#ResponseContentEnum for full list of supported values
+            // new List<IntegrationRequest.ResponseContentEnum> {
+            //     IntegrationRequest.ResponseContentEnum.CustomerSession,
+            //     IntegrationRequest.ResponseContentEnum.CustomerProfile
+            // }
             );
 
             // Create/update a customer session using `UpdateCustomerSessionV2Async` function
@@ -77,26 +80,63 @@ namespace _example
             Console.WriteLine(result);
 
             // Parsing the returned effects list, please consult https://developers.talon.one/Integration-API/handling-effects-v2 for the full list of effects and their corresponding properties
-            foreach (Effect effect in result.Effects) {
-                switch(effect.EffectType) {
+            foreach (Effect effect in result.Effects)
+            {
+                switch (effect.EffectType)
+                {
                     case "setDiscount":
                         // Initiating right props instance according to the effect type
-                        SetDiscountEffectProps setDiscountEffectProps = (SetDiscountEffectProps) Newtonsoft.Json.JsonConvert.DeserializeObject(effect.Props.ToString(), typeof(SetDiscountEffectProps));
+                        SetDiscountEffectProps setDiscountEffectProps = (SetDiscountEffectProps)Newtonsoft.Json.JsonConvert.DeserializeObject(effect.Props.ToString(), typeof(SetDiscountEffectProps));
 
                         // Access the specific effect's properties
                         Console.WriteLine("Set a discount '{0}' of {1:00.000}", setDiscountEffectProps.Name, setDiscountEffectProps.Value);
                         break;
                     // case "acceptCoupon":
-                        // AcceptCouponEffectProps acceptCouponEffectProps = (AcceptCouponEffectProps) Newtonsoft.Json.JsonConvert.DeserializeObject(effect.Props.ToString(), typeof(AcceptCouponEffectProps));
+                    // AcceptCouponEffectProps acceptCouponEffectProps = (AcceptCouponEffectProps) Newtonsoft.Json.JsonConvert.DeserializeObject(effect.Props.ToString(), typeof(AcceptCouponEffectProps));
 
-                        // Work with AcceptCouponEffectProps' properties
-                        // ...
-                        // break;
+                    // Work with AcceptCouponEffectProps' properties
+                    // ...
+                    // break;
                     default:
                         Console.WriteLine("Encounter unknown effect type: {0}", effect.EffectType);
                         break;
                 }
             }
+
+            //
+            // Run test for enum casing
+            //
+
+            Console.WriteLine("Testing Integration Request Enum casing issue");
+
+            string customerSession2Id = Guid.NewGuid().ToString();
+
+            // Intentionally no State set here to reproduce the nullable enum serialization path.
+            var customerSession2 = new NewCustomerSessionV2
+            {
+                CouponCodes = new List<string> { "JXHBAH5L" }
+            };
+
+            var integrationRequest = new IntegrationRequest(
+                customerSession2,
+                new List<IntegrationRequest.ResponseContentEnum>
+                {
+                    IntegrationRequest.ResponseContentEnum.Coupons,
+                }
+            );
+
+            IUpdateCustomerSessionV2ApiResponse? response2 =
+                await integrationApi.UpdateCustomerSessionV2Async(customerSession2Id, integrationRequest);
+
+            if (response2.IsBadRequest)
+            {
+                Console.WriteLine($"{response2.ReasonPhrase}{Environment.NewLine}{response2.RawContent}");
+                return;
+            }
+
+            Console.WriteLine("The response is ok");
+            IntegrationStateV2? result2 = response2.Ok();
+            Console.WriteLine(result2);
         }
     }
 }
